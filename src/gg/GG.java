3eba09ca2bb693393e38daea8b9bbcc8e6581e1a
Alfonso <Alfonso@DESKTOP-E34AEJF>
2026-02-21 16:18:34 +0800

    Failed attempt to use a function.

--- src/gg/GG.java
+++ src/gg/GG.java
@@ -3,8 +3,20 @@ import java.util.*;
 
 public class GG {
     
+    public static String performWish(){
+        int pityCounter = 0;
+        pityCounter++;
+        double chance = Math.random();
+        
+        if (pityCounter>=0){
+            pityCounter = 0;
+        }
+        
+        return "4 Star";
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Explorng Git");
+        performWish();
     }
     
 }
