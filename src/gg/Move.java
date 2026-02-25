42d98ebc8abc6eb13b89a1efac4fb85a1accc58f
Alfonso <Alfonso@DESKTOP-E34AEJF>
2026-02-26 00:23:36 +0800

    Added OOP

---
+++ src/gg/Move.java
@@ -0,0 +1,13 @@
+package gg;
+
+class Move {
+    String MoveName;
+    String MoveType;
+    int power;
+    
+    public Move(String name, String MoveType, int power){
+       this.MoveName = MoveName;
+       this.MoveType = MoveType;
+       this.power = power;
+    }
+}
---
+++ src/gg/Pokemon.java
@@ -0,0 +1,27 @@
+package gg;
+import java.util.*;
+
+class Pokemon {
+    String PokeName;
+    String PokeType;
+    int level;
+    int maxHp, hp, attack, spAttack, defense, spDefense,  speed;
+    ArrayList<Move> moves = new ArrayList<>();
+    
+    public Pokemon(String PokeName, String PokeType, int level, int bHP, int bAtk, int bSp_Atk, int bDef, int bSp_def,  int bSpd){
+        this.PokeName = PokeName;
+        this.PokeType = PokeType;
+        this.level = level;
+        
+        //STAT FORMULA PART LOL ;)
+        this.maxHp = ((bHP * 2 * level)/ 100) + 5;
+        this.hp = maxHp;
+        this.attack = ((bAtk * 2 * level)/ 100)+ 5;
+        this.spAttack = ((bSp_Atk * 2 * level)/ 100)+ 5;
+        this.defense = ((bDef * 2 * level)/ 100)+ 5;
+        this.spDefense = ((bSp_def * 2 * level)/ 100)+ 5;
+        this.speed = ((bSpd * 2 * level)/ 100)+ 5;
+    }
+    // PLANNING TO ADD DMG CALCULATION NEXT
+    
+}
